03b120ee2704379e52a86d7ba235792ccb5fddc4
CherryBoy <70771664+Evelin-jpg@users.noreply.github.com>
2020-12-16 12:06:37 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ClasesObjetosTest_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numNumerador1 = new System.Windows.Forms.NumericUpDown();
+            this.numDenominador1 = new System.Windows.Forms.NumericUpDown();
+            this.numDenominador2 = new System.Windows.Forms.NumericUpDown();
+            this.numNumerador2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCalcularResultado = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumerador1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDenominador1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDenominador2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumerador2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numNumerador1
+            // 
+            this.numNumerador1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numNumerador1.Location = new System.Drawing.Point(69, 65);
+            this.numNumerador1.Name = "numNumerador1";
+            this.numNumerador1.Size = new System.Drawing.Size(120, 38);
+            this.numNumerador1.TabIndex = 0;
+            // 
+            // numDenominador1
+            // 
+            this.numDenominador1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numDenominador1.Location = new System.Drawing.Point(69, 138);
+            this.numDenominador1.Name = "numDenominador1";
+            this.numDenominador1.Size = new System.Drawing.Size(120, 38);
+            this.numDenominador1.TabIndex = 1;
+            // 
+            // numDenominador2
+            // 
+            this.numDenominador2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numDenominador2.Location = new System.Drawing.Point(267, 138);
+            this.numDenominador2.Name = "numDenominador2";
+            this.numDenominador2.Size = new System.Drawing.Size(120, 38);
+            this.numDenominador2.TabIndex = 3;
+            // 
+            // numNumerador2
+            // 
+            this.numNumerador2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numNumerador2.Location = new System.Drawing.Point(267, 65);
+            this.numNumerador2.Name = "numNumerador2";
+            this.numNumerador2.Size = new System.Drawing.Size(120, 38);
+            this.numNumerador2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "____________________";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(264, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "____________________";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(218, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "+";
+            // 
+            // lblCalcularResultado
+            // 
+            this.lblCalcularResultado.AutoSize = true;
+            this.lblCalcularResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalcularResultado.Location = new System.Drawing.Point(414, 106);
+            this.lblCalcularResultado.Name = "lblCalcularResultado";
+            this.lblCalcularResultado.Size = new System.Drawing.Size(30, 31);
+            this.lblCalcularResultado.TabIndex = 7;
+            this.lblCalcularResultado.Text = "=";
+            this.lblCalcularResultado.Click += new System.EventHandler(this.lblCalcularResultado_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(463, 106);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(32, 31);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblCalcularResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numDenominador2);
+            this.Controls.Add(this.numNumerador2);
+            this.Controls.Add(this.numDenominador1);
+            this.Controls.Add(this.numNumerador1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numNumerador1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDenominador1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDenominador2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumerador2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numNumerador1;
+        private System.Windows.Forms.NumericUpDown numDenominador1;
+        private System.Windows.Forms.NumericUpDown numDenominador2;
+        private System.Windows.Forms.NumericUpDown numNumerador2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCalcularResultado;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Fraccion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesObjetosTest_01
+{
+    public class Fraccion
+    {
+        private int numerador;
+
+        public int Numerador
+        {
+            get
+            {
+                return numerador; 
+            }
+            set
+            {
+                numerador = value; 
+            }
+        }
+
+        private int denominador;
+
+        public int Denominador
+        {
+            get
+            {
+                return denominador;
+            }
+            set
+            {
+                denominador = value;
+            }
+        }
+
+        public Fraccion(int n, int d)
+        {
+            numerador = n;
+            denominador = d;
+        }
+
+        public Fraccion Sumar(Fraccion otra)
+        {
+            int denominadorComun = denominador * otra.denominador;
+            int n1 = (denominadorComun / denominador) * numerador;
+            int n2 = (denominadorComun / otra.denominador) * otra.denominador;
+            Fraccion resultado = new Fraccion(n1 + n2, denominadorComun);
+            return resultado;
+
+        }
+
+        public override string ToString()
+        {
+            return numerador + "/" + denominador;
+        }
+
+    }
+}
